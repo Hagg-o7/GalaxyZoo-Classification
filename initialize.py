abe9c31cba09b608be46fd5b930dd68ad2396c72
Hagg-o7 <f20220326@pilani.bits-pilani.ac.in>
2023-08-27 17:53:23 +0530

    updated initialize.py

--- initialize.py
+++ initialize.py
@@ -1,3 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import 
+import cv2 as cv
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+
+dim_check_img = r"/home/harshit/vscode/git/GalaxyZoo Classification/images_training_rev1/993040.jpg"
+test_img_example = cv.imread(dim_check_img)
+print(test_img_example.shape)
+
