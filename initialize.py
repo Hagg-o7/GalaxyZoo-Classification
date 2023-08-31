6e1a15ac41502a1bb36c79fa24c62085f8f18883
Hagg-o7 <f20220326@pilani.bits-pilani.ac.in>
2023-08-31 22:17:24 +0530

    added DataLoader

--- initialize.py
+++ initialize.py
@@ -1,14 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
+import pandas as pd
 import torch
-from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
 
 
 dim_check_img = r"/home/harshit/vscode/git/GalaxyZoo Classification/images_training_rev1/993040.jpg"
 test_img_example = cv.imread(dim_check_img)
 print(test_img_example.shape)
 
+batch_size = 64
+num_classes = 10
+learning_rate = 0.001
+num_epochs = 20
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+all_transforms = transforms.Compose([transforms.Resize((424,424,3)),
+                                     transforms.ToTensor(),
+                                     ])
+# Create Training dataset
+train_dataset = torchvision.datasets.CIFAR10(root = '/home/harshit/vscode/git/GalaxyZoo Classification/images_training_rev1',
+                                             train = True,
+                                             transform = all_transforms)
+
+# Create Testing dataset
+test_dataset = torchvision.datasets.CIFAR10(root = '/home/harshit/vscode/git/GalaxyZoo Classification/images_test_rev1',
+                                            train = False,
+                                            transform = all_transforms)
+
+# Instantiate loader objects to facilitate processing
+train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
+                                           batch_size = batch_size,
+                                           shuffle = True)
+
+
+test_loader = torch.utils.data.DataLoader(dataset = test_dataset,
+                                           batch_size = batch_size,
+                                           shuffle = True)
